8194d7dcdd3e769364b4549817d64877318e6f7e
Mandy Wade <lightninggirl413@hotmail.com>
2021-02-20 14:00:30 -0500

    Added Flag and Displays Win or Lose in Header

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,6 @@ export class App extends Component {
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     ],
-    state: 'new',
-    mines: 10,
-    difficulty: 0,
   }
 
   handleNewGame = async () => {
@@ -28,238 +25,92 @@ export class App extends Component {
     const game = await response.json()
     this.setState(game)
   }
-  handleClickOnCellCheck = async (row, column) => {
-    console.log(row, column)
-    // const url = `https://minesweeper-api.herokuapp.com/games/${this.state.id}/check`
-    const url = `https://minesweeper-api.herokuapp.com/games/1283/check`
-    const body = { row: row, column: column }
+  handleClickOnCellCheck = async (rowIndex, columnIndex) => {
+    if (
+      this.state.id === undefined ||
+      this.state.state === 'won' ||
+      this.state.state === 'lost'
+    ) {
+      return
+    }
+
+    const url = `https://minesweeper-api.herokuapp.com/games/${this.state.id}/check`
+    const body = { row: rowIndex, col: columnIndex }
     const response = await fetch(url, {
       method: 'POST',
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify(body),
     })
-    console.log(response)
     const game = await response.json()
-    console.log(game)
+    this.setState(game)
+  }
+
+  handleClickOnCellFlag = async (rowIndex, columnIndex) => {
+    if (
+      this.state.id === undefined ||
+      this.state.state === 'won' ||
+      this.state.state === 'lost'
+    ) {
+      return
+    }
+
+    const url = `https://minesweeper-api.herokuapp.com/games/${this.state.id}/flag`
+    const body = { row: rowIndex, col: columnIndex }
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+    const game = await response.json()
     this.setState(game)
   }
 
   render() {
-    console.log(this.state)
+    let header = 'Minesweeper'
+    if (this.state.state === 'won') {
+      header = 'Minesweeper - You WON!!!!'
+    }
+    if (this.state.state === 'lost') {
+      header = 'Minesweeper - You LOST!'
+    }
+
     return (
       <div className="gameboard">
         <header>
-          Minesweeper - <button onClick={this.handleNewGame}>New Game</button>
+          <h1>{header}</h1>
         </header>
+        <article>
+          <button onClick={this.handleNewGame}>New Game</button>
+        </article>
         <section>
-          <ul>
-            {this.state.board.map((boardRow, rowIndex) => {
-              return boardRow.map((cell, columnIndex) => {
+          <table>
+            <thead></thead>
+            <tbody>
+              {this.state.board.map((boardRow, rowIndex) => {
                 return (
-                  <li
-                    key={columnIndex}
-                    onClick={() =>
-                      this.handleClickOnCellCheck(rowIndex, columnIndex)
-                    }
-                  >
-                    {cell}
-                  </li>
+                  <tr key={rowIndex}>
+                    {boardRow.map((cell, columnIndex) => {
+                      return (
+                        <td
+                          className="cell"
+                          key={columnIndex}
+                          onClick={() =>
+                            this.handleClickOnCellCheck(rowIndex, columnIndex)
+                          }
+                          onContextMenu={e => {
+                            e.preventDefault()
+                            this.handleClickOnCellFlag(rowIndex, columnIndex)
+                          }}
+                        >
+                          {cell}
+                        </td>
+                      )
+                    })}
+                  </tr>
                 )
-              })
-            })}
-            {/* <li onClick={() => this.handleClickOnCellCheck(0, 0)}> */}
-            {/* {this.state.board[0][0]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(0, 1)}>
-              {this.state.board[0][1]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(0, 2)}>
-              {this.state.board[0][2]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(0, 3)}>
-              {this.state.board[0][3]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(0, 4)}>
-              {this.state.board[0][4]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(0, 5)}>
-              {this.state.board[0][5]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(0, 6)}>
-              {this.state.board[0][6]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(0, 7)}>
-              {this.state.board[0][7]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(1, 0)}>
-              {this.state.board[1][0]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(1, 1)}>
-              {this.state.board[1][1]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(1, 2)}>
-              {this.state.board[1][2]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(1, 3)}>
-              {this.state.board[1][3]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(1, 4)}>
-              {this.state.board[1][4]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(1, 5)}>
-              {this.state.board[1][5]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(1, 6)}>
-              {this.state.board[1][6]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(1, 7)}>
-              {this.state.board[1][7]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(2, 0)}>
-              {this.state.board[2][0]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(2, 1)}>
-              {this.state.board[2][1]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(2, 2)}>
-              {this.state.board[2][2]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(2, 3)}>
-              {this.state.board[2][3]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(2, 4)}>
-              {this.state.board[2][4]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(2, 5)}>
-              {this.state.board[2][5]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(2, 6)}>
-              {this.state.board[2][6]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(2, 7)}>
-              {this.state.board[2][7]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(3, 0)}>
-              {this.state.board[3][0]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(3, 1)}>
-              {this.state.board[3][1]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(3, 2)}>
-              {this.state.board[3][2]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(3, 3)}>
-              {this.state.board[3][3]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(3, 4)}>
-              {this.state.board[3][4]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(3, 5)}>
-              {this.state.board[3][5]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(3, 6)}>
-              {this.state.board[3][6]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(3, 7)}>
-              {this.state.board[3][7]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(4, 0)}>
-              {this.state.board[4][0]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(4, 1)}>
-              {this.state.board[4][1]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(4, 2)}>
-              {this.state.board[4][2]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(4, 3)}>
-              {this.state.board[4][3]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(4, 4)}>
-              {this.state.board[4][4]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(4, 5)}>
-              {this.state.board[4][5]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(4, 6)}>
-              {this.state.board[4][6]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(4, 7)}>
-              {this.state.board[4][7]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(5, 0)}>
-              {this.state.board[5][0]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(5, 1)}>
-              {this.state.board[5][1]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(5, 2)}>
-              {this.state.board[5][2]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(5, 3)}>
-              {this.state.board[5][3]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(5, 4)}>
-              {this.state.board[5][4]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(5, 5)}>
-              {this.state.board[5][5]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(5, 6)}>
-              {this.state.board[5][6]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(5, 7)}>
-              {this.state.board[5][7]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(6, 0)}>
-              {this.state.board[6][0]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(6, 1)}>
-              {this.state.board[6][1]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(6, 2)}>
-              {this.state.board[6][2]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(6, 3)}>
-              {this.state.board[6][3]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(6, 4)}>
-              {this.state.board[6][4]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(6, 5)}>
-              {this.state.board[6][5]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(6, 6)}>
-              {this.state.board[6][6]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(7, 7)}>
-              {this.state.board[6][7]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(7, 0)}>
-              {this.state.board[7][0]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(7, 1)}>
-              {this.state.board[7][1]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(7, 2)}>
-              {this.state.board[7][2]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(7, 3)}>
-              {this.state.board[7][3]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(7, 4)}>
-              {this.state.board[7][4]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(7, 5)}>
-              {this.state.board[7][5]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(7, 6)}>
-              {this.state.board[7][6]}
-            </li>
-            <li onClick={() => this.handleClickOnCellCheck(7, 7)}>
-              {this.state.board[7][7]}
-            </li> */}
-          </ul>
+              })}
+            </tbody>
+          </table>
         </section>
         <footer>
           <p>This game of Minesweeper was created by Mandy Wade</p>
